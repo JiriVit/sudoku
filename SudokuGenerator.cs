c83fe9c7d6fdd99c104c573730144213e3dbd690
Jiri Vit <jiri.vit@windowslive.com>
2025-06-19 07:50:11 +0200

    Cleanup in SudokuGenerator.cs.

--- SudokuGenerator.cs
+++ SudokuGenerator.cs
@@ -25,6 +25,7 @@ namespace Sudoku
 
         private static readonly Random random = new();
         private static readonly int[,] sudoku = new int[9, 9];
+        private static readonly List<int> numbersInRegion = [];
 
         #endregion
 
@@ -35,7 +36,7 @@ namespace Sudoku
         /// </summary>
         public static void Generate()
         {
-            Reset();
+            Array.Clear(sudoku);
             AddNextNumber(0, 0);
         }
 
@@ -43,136 +44,124 @@ namespace Sudoku
 
         #region .: Private Methods :.
 
-        private static void Reset()
-        {
-            for (int row = 0; row < 9; row++)
-            {
-                for (int col = 0; col < 9; col++)
-                {
-                    sudoku[row, col] = 0;
-                }
-            }
-        }
-
+        /// <summary>
+        /// Adds next number to the sudoku and validates it.
+        /// Recursively calls itself to fill the grid or to backtrack in case of wrong path.
+        /// </summary>
+        /// <param name="row">Row number of the cell to be filled (0-8).</param>
+        /// <param name="col">Column number of the cell to be filled (0-8).</param>
+        /// <returns>TRUE if all the sudoku has been filled, FALSE if no valid number has been found.</returns>
         private static bool AddNextNumber(int row, int col)
         {
             List<int> availableNumbers = [.. Enumerable.Range(1, 9)];
             bool valid = true;
-            bool backtrack = false;
 
             bool getAnotherNumber = true;
             while (getAnotherNumber)
             {
+                // pick a random available number, then remove it from available numbers
                 int index = random.Next(availableNumbers.Count);
                 sudoku[row, col] = availableNumbers[index];
                 availableNumbers.RemoveAt(index);
 
-                valid = Validate();
-                backtrack = false;
-
+                // if the number doesn't invalidate the sudoku, recursively call this method
+                valid = IsSudokuValid();
                 if (valid)
                 {
                     bool lastCell = (row == 8) && (col == 8);
 
                     if (!lastCell)
                     {
-                        int newCol = (col + 1) % 9;
-                        int newRow = (newCol == 0) ? (row + 1) : row;
+                        int nextCellRow = (col + 1) % 9;
+                        int nextCellCol = (nextCellRow == 0) ? (row + 1) : row;
 
-                        backtrack = !AddNextNumber(newRow, newCol);
+                        valid = AddNextNumber(nextCellCol, nextCellRow);
                     }
                 }
 
-                getAnotherNumber = (!valid || backtrack) && (availableNumbers.Count > 0);
+                getAnotherNumber = !valid && (availableNumbers.Count > 0);
             }
 
-            bool fail = (!valid || backtrack) && (availableNumbers.Count == 0);
-
-            if (fail)
+            // if we run out of numbers without finding a valid one, we need to backtrack
+            bool backtrack = !valid && (availableNumbers.Count == 0);
+            if (backtrack)
             {
                 sudoku[row, col] = 0;
             }
 
-            return !fail;
+            return !backtrack;
         }
 
-        public static bool Validate()
+        /// <summary>
+        /// Checks if the current sudoku (excluding empty cells) is valid, ie. doesn't
+        /// violate rules for solved sudoku.
+        /// </summary>
+        /// <returns>Boolean value indicating validity.</returns>
+        public static bool IsSudokuValid()
         {
-            bool valid = true;
-            bool stop = false;
+            bool isValid = true;
 
             // check rows
-            for (int row = 0; (row < 9) && !stop; row++)
+            for (int row = 0; (row < 9) && isValid; row++)
             {
-                List<int> numbers = [];
-
-                for (int col = 0; (col < 9) && !stop; col++)
+                numbersInRegion.Clear();
+                for (int col = 0; (col < 9) && isValid; col++)
                 {
-                    int n = sudoku[row, col];
-                    if (n > 0)
-                    {
-                        if (!numbers.Contains(n))
-                        {
-                            numbers.Add(n);
-                        }
-                        else
-                        {
-                            valid = false;
-                            stop = true;
-                        }
-                    }
+                    isValid = !IsDuplicateNumber(row, col);
                 }
             }
 
             // check columns
-            for (int col = 0; (col < 9) && !stop; col++)
+            for (int col = 0; (col < 9) && isValid; col++)
             {
-                List<int> numbers = [];
-
-                for (int row = 0; (row < 9) && !stop; row++)
+                numbersInRegion.Clear();
+                for (int row = 0; (row < 9) && isValid; row++)
                 {
-                    int n = sudoku[row, col];
-                    if (n > 0)
-                    {
-                        if (!numbers.Contains(n))
-                        {
-                            numbers.Add(n);
-                        }
-                        else
-                        {
-                            valid = false;
-                            stop = true;
-                        }
-                    }
+                    isValid = !IsDuplicateNumber(row, col);
                 }
             }
 
             // check subgrids
-            for (int sg = 0; (sg < 9) && !stop; sg++)
+            for (int subgrid = 0; (subgrid < 9) && isValid; subgrid++)
             {
-                List<int> numbers = [];
-
-                for (int cell = 0; (cell < 9) && !stop; cell++)
+                numbersInRegion.Clear();
+                for (int subgridCell = 0; (subgridCell < 9) && isValid; subgridCell++)
                 {
-                    int row = 3 * (sg / 3) + cell / 3;
-                    int col = 3 * (sg % 3) + cell % 3;
-                    int n = sudoku[row, col];
-                    if (n > 0)
-                    {
-                        if (!numbers.Contains(n))
-                        {
-                            numbers.Add(n);
-                        }
-                        else
-                        {
-                            valid = false;
-                            stop = true;
-                        }
-                    }
+                    int row = 3 * (subgrid / 3) + subgridCell / 3;
+                    int col = 3 * (subgrid % 3) + subgridCell % 3;
+                    isValid = !IsDuplicateNumber(row, col);
                 }
             }
 
-            return valid;
+            return isValid;
+        }
+
+        /// <summary>
+        /// Checks if the number in a cell is duplicate with others in the region.
+        /// Uses static list <see cref="numbersInRegion"/>, it needs to be cleared before starting
+        /// with another region.
+        /// </summary>
+        /// <param name="row">Row index of the checked cell (0-8).</param>
+        /// <param name="col">Column index of the checked cell (0-8).</param>
+        /// <returns>Boolean value indicated duplicate number.</returns>
+        private static bool IsDuplicateNumber(int row, int col)
+        {
+            bool duplicate = false;
+
+            int number = sudoku[row, col];
+            if (number > 0)
+            {
+                if (!numbersInRegion.Contains(number))
+                {
+                    numbersInRegion.Add(number);
+                }
+                else
+                {
+                    duplicate = true;
+                }
+            }
+
+            return duplicate;
         }
 
         #endregion
